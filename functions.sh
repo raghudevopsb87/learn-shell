10a888a49625ee6490af4cac0d06718c54e13509
Raghu K <raghuk.vit@gmail.com>
2025-11-21 07:43:41 +0530

    Variables intro

--- functions.sh
+++ functions.sh
@@ -1,17 +1,29 @@
-# Declaring the function
+## Declaring the function
+#sample() {
+#  echo Sample Function
+#}
+#
+## Calling the function
+#sample
+#
+#
+## Source common.sh script. Meaning load the functions and variables inside that script to here
+#source common.sh
+#sample1
+#
+## Access variables across functions.
+#a=10
+#sample2
+#echo b - $b
+
+
+
 sample() {
-  echo Sample Function
+  echo x - $x
 }
 
-# Calling the function
+x=10
 sample
 
-
-# Source common.sh script. Meaning load the functions and variables inside that script to here
-source common.sh
-sample1
-
-# Access variables across functions.
-a=10
-sample2
-echo b - $b
+x=20
+sample
